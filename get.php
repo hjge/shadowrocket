271171e2ad11e379f79079408c0721af21780747
hjge <100683403+hjge@users.noreply.github.com>
2022-03-01 20:57:36 +0800

    Add files via upload

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+$ch = curl_init();
+
+$url1 = "https://api.shadowrocket.top/sms.php?authCode=";
+$url2 = $_GET["authCode"];
+curl_setopt($ch, CURLOPT_URL, $url1.$url2);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$output = curl_exec($ch);
+curl_close($ch);
+print_r($output);
+?>
